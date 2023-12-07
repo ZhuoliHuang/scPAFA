f0d27f1c689f7e4ea9fce9f1f9f061d715f0f65a
ZhuoliHuang <bioagr_huangzl@163.com>
2023-12-07 14:27:30 +0800

    run_mofapy2

--- scPAFA/_pseudobulk/_Mofa.py
+++ scPAFA/_pseudobulk/_Mofa.py
@@ -3,7 +3,9 @@ from scPAFA.run.entry_point import entry_point
 
 def run_mofapy2(
  long_table:pd.DataFrame,
+ scale_groups:bool = False,
  scale_views:bool = False,
+ center_groups:bool=True,
  factor_number:int = 10,
  random_seed:int = 0,
  spikeslab_factors:bool = False,
@@ -16,11 +18,13 @@ def run_mofapy2(
    A wrap function to run the MOFAPY2 and return the MOFA object.
    For detailed parameters and usage, please refer to the MOFAPY2 documentation.
    https://github.com/bioFAM/mofapy2/blob/master/mofapy2/notebooks/getting_started_python.ipynb
-
+   
    Parameters
    ----------
    long_table : pd.DataFrame
       The long-format table containing the data. 
+   scale_groups : bool, optional
+      Whether to scale the groups.if groups have different ranges/variances, it is good practice to scale each group to unit variance. Default is False.
    scale_views : bool, optional
       Whether to scale the views.if views have different ranges/variances, it is good practice to scale each view to unit variance. Default is False.
    factor_number : int
@@ -30,7 +34,7 @@ def run_mofapy2(
    spikeslab_weights : bool, optional
       use spike-slab sparsity prior in the weights? Default is TRUE.
    ard_factors :  bool, optional
-      use ARD prior in the weights? Default is False. If using multigroup framework, must set to True.
+      use ARD prior in the weights? Default is False. If using multigroup framework, should set to True.
    ard_weights :  bool, optional
       use ARD prior in the weights? Default is TRUE if using multiple views.
    random_seed : int, optional
@@ -46,7 +50,7 @@ def run_mofapy2(
       MOFA object.
    """
    ent = entry_point()
-   ent.set_data_options(scale_views = scale_views)
+   ent.set_data_options(scale_groups=scale_groups,scale_views = scale_views,center_groups=center_groups)
    ent.set_data_df(long_table.copy()) # Use "gaussian" (default) likelihoods , which are suitable for pseudobulk PAS data
    ent.set_model_options(factors = factor_number,spikeslab_factors=spikeslab_factors,spikeslab_weights = spikeslab_weights,ard_factors = ard_factors,ard_weights =ard_weights)
    ent.set_train_options(convergence_mode = convergence_mode,dropR2 = dropR2,seed = random_seed)
